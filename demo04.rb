89927eda2c6e5a6150799bbf9b32b6cc0cdd0c26
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-08-15 19:16:54 -0400

    Added a fourth hello demo with license info.

---
+++ demo04.rb
@@ -0,0 +1,31 @@
+# Encoding: UTF-8
+
+require 'gosu'
+
+
+class Welcome < Gosu::Window
+  WIDTH   = 800
+  HEIGHT  = 600
+  PADDING = 20
+
+  def initialize
+    super WIDTH, HEIGHT
+
+    self.caption = "Gosu License Info - Press Esc to Exit."
+
+    text = Gosu::LICENSES.split(",").join("\n")
+
+    @text = Gosu::Image.from_text(text, 22)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
+  end
+
+  def draw
+    @text.draw(PADDING, PADDING, 0)
+  end
+
+end
+
+Welcome.new.show
