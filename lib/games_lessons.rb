553dadb2cc849eba5da4eb7fd10c7f598d4dc799
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-15 20:41:23 -0400

    Now able to run some programs.

--- lib/games_lessons.rb
+++ lib/games_lessons.rb
@@ -4,12 +4,29 @@ require_relative 'games_lessons/version'
 # The master control class for the games lessons.
 class GamesLessons
 
+  CLIST = ['01']
+
   # Set up the games lessons data.
   def initialize
+    @prog_list = { }
+    base_path = File.dirname(File.absolute_path(__FILE__))
+
+    CLIST.each do |dir|
+      name = "#{base_path}/../#{dir}/*.rb"
+      Dir[name].each do |full|
+        @prog_list[File.basename(full, '.rb')] = full
+      end
+    end
+
   end
 
   # The main entry point for the games lessons application.
-  def main(args)
+  def main(args=nil)
+    require @prog_list[args[0]]
+  rescue
+    puts "The program #{args[0]} was not found."
   end
 
 end
+
+GamesLessons.new.main(ARGV)
