7e4b1ffc07b79a1526f934663817f8ac710634e7
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-15 21:02:26 -0400

    Refactored the chapter list.

--- lib/games_lessons.rb
+++ lib/games_lessons.rb
@@ -4,14 +4,13 @@ require_relative 'games_lessons/version'
 # The master control class for the games lessons.
 class GamesLessons
 
-  CLIST = ['01']
-
   # Set up the games lessons data.
   def initialize
     @prog_list = { }
     base_path = File.dirname(File.absolute_path(__FILE__))
+    chapter_list = ['01']
 
-    CLIST.each do |dir|
+    chapter_list.each do |dir|
       name = "#{base_path}/../#{dir}/*.rb"
       Dir[name].each do |full|
         @prog_list[File.basename(full, '.rb')] = full
