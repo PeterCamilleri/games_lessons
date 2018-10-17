9ee0c414e1d9c6cdea44eec5f902d87d47a3e45b
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-17 18:03:16 -0400

    Added the angles program for chapter 02.

---
+++ 02/gosu_angles.rb
@@ -0,0 +1,53 @@
+# Angular data in Gosu
+
+require "gosu"
+
+class Angles < Gosu::Window
+  def initialize(width=800, height=600, fullscreen=false)
+    super
+    self.caption = "Hello World - Press Esc to Exit."
+
+    @margin  = 20
+    @counter = 0
+    @limit   = 20
+    @buffer = ["", "", "", ""]
+
+    @arrow = Gosu::Image.from_text("→", 200)
+    @gosu = 0
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
+  end
+
+  def update
+    if @counter == @limit
+      @counter = 0
+      @gosu = (@gosu - 5) % 360
+    else
+      @counter += 1
+    end
+
+    degrees = -@gosu % 360
+    radians = degrees.degrees_to_radians
+    pi_rads = (radians/Math::PI).rationalize(0.01)
+
+    @buffer = ["Gosu: #{@gosu}",
+               "Degrees: #{degrees}",
+               "Radians: #{radians}",
+               "PI Rads: #{pi_rads}"
+              ]
+
+    @legend = Gosu::Image.from_text(@buffer.join("\n"), 32)
+
+    @x = self.width/2
+    @y = self.height/2
+  end
+
+  def draw
+    @legend.draw(@margin, @margin, 0)
+    @arrow.draw_rot(@x, @y, 0, @gosu)
+  end
+end
+
+Angles.new.show
