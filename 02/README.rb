40afe550c3a9a4efaec19fb84749a43433795647
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-17 23:12:56 -0400

    Fixed name of read me file.

---
+++ 02/README.rb
@@ -0,0 +1,7 @@
+# Dimensions of Gosu
+
+In this section we take a look at how Gosu deals with matters like pixels,
+angles and time. All of these have a profound impact on the logic of the games
+we want to write. In particular we will examine how we can manage these
+dimensions and make them work with us, rather than being a source of defects
+and frustration.
