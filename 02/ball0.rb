e69e868c02a551f85c2073d92d994cf1d708dbde
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-26 13:25:55 -0400

    The ball0 program does not need the system mouse pointer.

--- 02/ball0.rb
+++ 02/ball0.rb
@@ -16,10 +16,6 @@ class Ball < Gosu::Window
     self.caption = "A Ball. Press Escape to exit."
   end
 
-  def needs_cursor?
-    true
-  end
-
   def button_down(id)
     close if id == Gosu::KB_ESCAPE
   end
